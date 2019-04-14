086e42213b4683f10ca45074b9e7bc91edaa2c09
Gergő Bakonyi <hakuhun@gmail.com>
2019-04-14 14:41:00 +0200

    dynamic initialization is not supported for __device__ variable

--- oe-pso/kernel.cu
+++ oe-pso/kernel.cu
@@ -5,6 +5,7 @@
 #include "vector_functions.h"
 #include <curand_kernel.h>
 #include <ctime>
+#include <helper_cuda.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,63 +22,29 @@
 
 using namespace std;
 
-class Managed {
-public:
-	void *operator new(size_t len) {
-		void *ptr;
-		cudaMallocManaged(&ptr, len);
-		cudaDeviceSynchronize();
-		return ptr;
-	}
-
-	void operator delete(void *ptr) {
-		cudaDeviceSynchronize();
-		cudaFree(ptr);
-	}
-};
-
-//class Location : public Managed
-//{
+//class Managed {
 //public:
-//	float x;
-//	float y;
 //
-//	Location() {}
 //
-//	Location(float x, float y) {
-//		this->x = x;
-//		this->y = y;
+//
+//	void *operator new(size_t len) {
+//		void *ptr;
+//		cudaMallocManaged(&ptr, len);
+//		cudaDeviceSynchronize();
+//		return ptr;
 //	}
 //
-//	Location operator=(const Location &mng) {
-//		x = mng.x;
-//		y = mng.y;
-//		return *this;
-//	}
-//
-//	//Két vektor különbsége
-//	Location operator-(const Location &mng) {
-//		return Location(this->x - mng.x, this->y - mng.y);
-//	}
-//
-//	//Két vektor összege
-//	Location operator+(const Location * mng) {
-//		Location result = Location(this->x + mng->x, this->y + mng->y);
-//		return result;
-//	}
-//
-//	//számmal való szorzás
-//	Location operator*(const int &number) {
-//		return Location(this->x * number, this->y * number);
+//	void operator delete(void *ptr) {
+//		cudaDeviceSynchronize();
+//		cudaFree(ptr);
 //	}
 //};
 
-class Particle : public Managed
+class Particle /*: public Managed*/
 {
 public:
-
-	Particle() {
-
+	__host__ __device__ Particle() {
+		velocity = make_float2(0, 0);
 	}
 
 	//vector of the current location
@@ -87,22 +54,25 @@ public:
 	//vector of the position where the particle is heading to
 	float2 direction;
 
-	float2 velocity = make_float2(0, 0);
+	float2 velocity;
 };
 
-__device__ Particle * dev_particles[N];
+__device__ Particle dev_particles[N];
 Particle host_particles[N];
 
-__shared__ float2 dev_globalOptimum;
+__device__ float2 dev_globalOptimum;
+
+int asd5[N];
+__device__ int dasd[N];
 
 //Innertial coefficent (innerciális együttható)
-__device__ float w = 0.5;
+__constant__ float w = 0.5;
 
 //Acceleration coefficent (gyorsítási együttható)
-__device__ float c1 = 0.2;
+__constant__ float c1 = 0.2;
 
 //Acceleration coefficent (gyorsítási együttható)
-__device__ float c2 = 0.2;
+__constant__ float c2 = 0.2;
 
 inline __host__ __device__ float2 operator-(float2 a, float2 b)
 {
@@ -139,7 +109,7 @@ __device__ double DistanceCalculate(float2 a, float2 b)
 __global__ void Evaluation() {
 
 	int index = blockDim.x * blockIdx.x + threadIdx.x;
-	Particle * particle = dev_particles[index];
+	Particle * particle = &dev_particles[index];
 
 	if (DistanceCalculate(particle->direction, particle->localOptimum) <
 		DistanceCalculate(particle->direction - particle->velocity, particle->localOptimum))
@@ -165,19 +135,26 @@ __device__ float cudaRand()
 
 __global__ void CalculateVelocity() {
 	int index = blockDim.x * blockIdx.x + threadIdx.x;
-	Particle * particle = dev_particles[index];
+	Particle * particle = &dev_particles[index];
 
 	//Calculate the velocity
 	particle->velocity = w * particle->velocity
 		+ cudaRand() * c1 * (particle->localOptimum - particle->direction)
 		+ cudaRand() * c2 * (dev_globalOptimum - particle->direction);
+	printf("Value is: %d\n", particle->velocity.x);
 }
 
 __global__ void CalculateNewDirection() {
 	int index = blockDim.x * blockIdx.x + threadIdx.x;
-	Particle * particle = dev_particles[index];
+	Particle * particle = &dev_particles[index];
 
 	particle->direction = particle->direction + particle->velocity;
+}
+
+__global__ void checkParticles(){
+	int index = blockDim.x * blockIdx.x + threadIdx.x;
+	Particle * particle = &dev_particles[index];
+	printf("Value is: %d\n", particle->direction.x);
 }
 
 void initParticles() {
@@ -188,17 +165,19 @@ void initParticles() {
 		host_particles[i].position = make_float2(RANDOM(MIN2, MAX2), RANDOM(MIN2, MAX2));
 		host_particles[i].localOptimum = make_float2(RANDOM(MIN2, MAX2), RANDOM(MIN2, MAX2));
 		host_particles[i].direction = make_float2(RANDOM(MIN2, MAX2), RANDOM(MIN2, MAX2));
+		asd5[i] = RANDOM(0, 100);
 	}
 }
 
 void checkError() {
-	cudaError_t cudaStatus;
-
-	cudaStatus = cudaSetDevice(0);
+	cudaError_t cudaStatus = cudaGetLastError();	
 	if (cudaStatus != cudaSuccess) {
-		cout << stderr, cudaGetErrorName(cudaStatus);
+		cout << "Hiba: " << cudaGetErrorName(cudaStatus) << '\n';
+		cout << cudaGetErrorString(cudaStatus);
 	}
 }
+
+
 
 int main()
 {
@@ -208,9 +187,15 @@ int main()
 	//copy particles from host to device
 	cudaMemcpyToSymbol(dev_particles, host_particles, N * sizeof(Particle));
 	checkError();
+
 	//initalize global optimum variable
 	float2 host_gOptimum = make_float2(1, 1);
 	cudaMemcpyToSymbol(&dev_globalOptimum, &host_gOptimum, N * sizeof(float2));
+
+	checkError();
+	cudaMemcpyToSymbol(dasd, asd5, N * sizeof(int));
+
+	checkParticles << <1, N >> > ();
 	checkError();
 
 	Evaluation << <1, N >> > ();
